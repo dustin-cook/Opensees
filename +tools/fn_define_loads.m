881068d67911a16302fdbef6ecd7ad6f679b5eb9
Dustin Cook <dustin@hbrisk.com>
2018-05-01 10:04:52 -0600

    Reworks the way ground motion inputs are handled

--- +tools/fn_define_loads.m
+++ +tools/fn_define_loads.m
@@ -1,10 +1,15 @@
-function [ ] = fn_define_loads( output_dir, analysis, damp_ratio, node, ground_motion )
+function [ ground_motion ] = fn_define_loads( output_dir, analysis, damp_ratio, node )
 %UNTITLED8 Summary of this function goes here
 %   Detailed explanation goes here
 
 % Write Loads File
 file_name = [output_dir filesep 'loads.tcl'];
 fileID = fopen(file_name,'w');
+
+%% Load ground motion data
+gm_seq_table = readtable(['inputs' filesep 'ground_motion_sequence.csv'],'ReadVariableNames',true);
+ground_motion_seq = gm_seq_table(gm_seq_table.id == analysis.gm_seq_id,:);
+ground_motion_table = readtable(['inputs' filesep 'ground_motion.csv'],'ReadVariableNames',true);
 
 %% Define Gravity Loads (node id, axial, shear, moment)
 fprintf(fileID,'pattern Plain 1 Linear {  \n');
@@ -40,46 +45,48 @@ fprintf(fileID,'loadConst -time 0.0 \n');
 % end
 % fprintf(fileID,'} \n');
 
-%% For Dynamic Analysis
-if analysis.type == 3 || analysis.type == 4
-    % Define Seismic Excitation Load
-    % timeSeries Path $tag -dt $dt -filePath $filePath <-factor $cFactor> <-useLast> <-prependZero> <-startTime $tStart>
-    % pattern UniformExcitation $patternTag $dir -accel $tsTag <-vel0 $vel0> <-fact $cFactor>
-    if ~strcmp(ground_motion.eq_name_x,'')
-        fprintf(fileID,'timeSeries Path 1 -dt %f -filePath %s/%s -factor 386. \n',ground_motion.eq_dt, ground_motion.eq_dir{1}, ground_motion.eq_name_x{1});
-        fprintf(fileID,'pattern UniformExcitation 3 1 -accel 1 -fact %f \n',ground_motion.x_ratio); 
-    end
-    if ~strcmp(ground_motion.eq_name_z,'')
-        fprintf(fileID,'timeSeries Path 2 -dt %f -filePath %s/%s -factor 386. \n',ground_motion.eq_dt, ground_motion.eq_dir{1}, ground_motion.eq_name_z{1});
-        fprintf(fileID,'pattern UniformExcitation 4 3 -accel 2 -fact %f \n',ground_motion.z_ratio); 
-    end
-    if ~strcmp(ground_motion.eq_name_y,'')
-        fprintf(fileID,'timeSeries Path 3 -dt %f -filePath %s/%s -factor 386. \n',ground_motion.eq_dt, ground_motion.eq_dir{1}, ground_motion.eq_name_y{1});
-        fprintf(fileID,'pattern UniformExcitation 5 2 -accel 3 -fact %f \n',ground_motion.y_ratio); 
-    end
-
-    % Define Damping based on eigen modes
-    fprintf(fileID,'set lambda [eigen -fullGenLapack 6] \n');
-    fprintf(fileID,'puts $lambda \n');
-    fprintf(fileID,'set pi 3.141593\n');
-    fprintf(fileID,'set i 0 \n');
-    fprintf(fileID,'foreach lam $lambda {\n');
-    fprintf(fileID,'    set i [expr $i+1] \n');
-    fprintf(fileID,'	set omega($i) [expr sqrt($lam)]\n');
-    fprintf(fileID,'	set period($i) [expr 2*$pi/sqrt($lam)]\n');
-    fprintf(fileID,'}\n');
-    fprintf(fileID,'puts $period(1) \n');
-    fprintf(fileID,'set alpha [expr 2*%d*(1-$omega(1))/(1/$omega(1) - $omega(1)/($omega(3)*$omega(3)))]\n', damp_ratio);
-    fprintf(fileID,'set beta [expr 2*%d - $alpha/($omega(3)*$omega(3))]\n', damp_ratio);
-    if strcmp(analysis.damping,'rayleigh')
-        fprintf(fileID,'rayleigh $alpha 0 $beta 0 \n'); 
-    elseif strcmp(analysis.damping,'modal')
-        fprintf(fileID,'modalDamping %d \n',damp_ratio); 
-        fprintf(fileID,'rayleigh 0 $beta 0 0 \n'); 
-    else
-        error('Damping Type Not Recognized')
-    end
+%% Dynamic Analysis
+% Define Seismic Excitation Load
+% timeSeries Path $tag -dt $dt -filePath $filePath <-factor $cFactor> <-useLast> <-prependZero> <-startTime $tStart>
+% pattern UniformExcitation $patternTag $dir -accel $tsTag <-vel0 $vel0> <-fact $cFactor>
+if ground_motion_seq.eq_id_x ~= 0
+    ground_motion.x = ground_motion_table(ground_motion_table.id == ground_motion_seq.eq_id_x,:);
+    fprintf(fileID,'timeSeries Path 1 -dt %f -filePath %s/%s -factor 386. \n',ground_motion.x.eq_dt, ground_motion.x.eq_dir{1}, ground_motion.x.eq_name_x{1});
+    fprintf(fileID,'pattern UniformExcitation 3 1 -accel 1 -fact %f \n',ground_motion_seq.x_ratio); 
 end
+if ground_motion_seq.eq_id_z ~= 0
+    ground_motion.z = ground_motion_table(ground_motion_table.id == ground_motion_seq.eq_id_z,:);
+    fprintf(fileID,'timeSeries Path 2 -dt %f -filePath %s/%s -factor 386. \n',ground_motion.z.eq_dt, ground_motion.z.eq_dir{1}, ground_motion.z.eq_name_z{1});
+    fprintf(fileID,'pattern UniformExcitation 4 3 -accel 2 -fact %f \n',ground_motion_seq.z_ratio); 
+end
+if ground_motion_seq.eq_id_y ~= 0
+    ground_motion.y = ground_motion_table(ground_motion_table.id == ground_motion_seq.eq_id_y,:);
+    fprintf(fileID,'timeSeries Path 3 -dt %f -filePath %s/%s -factor 386. \n',ground_motion.y.eq_dt, ground_motion.y.eq_dir{1}, ground_motion.y.eq_name_y{1});
+    fprintf(fileID,'pattern UniformExcitation 5 2 -accel 3 -fact %f \n',ground_motion_seq.y_ratio); 
+end
+
+% Define Damping based on eigen modes
+fprintf(fileID,'set lambda [eigen -fullGenLapack 6] \n');
+fprintf(fileID,'puts $lambda \n');
+fprintf(fileID,'set pi 3.141593\n');
+fprintf(fileID,'set i 0 \n');
+fprintf(fileID,'foreach lam $lambda {\n');
+fprintf(fileID,'    set i [expr $i+1] \n');
+fprintf(fileID,'	set omega($i) [expr sqrt($lam)]\n');
+fprintf(fileID,'	set period($i) [expr 2*$pi/sqrt($lam)]\n');
+fprintf(fileID,'}\n');
+fprintf(fileID,'puts $period(1) \n');
+fprintf(fileID,'set alpha [expr 2*%d*(1-$omega(1))/(1/$omega(1) - $omega(1)/($omega(3)*$omega(3)))]\n', damp_ratio);
+fprintf(fileID,'set beta [expr 2*%d - $alpha/($omega(3)*$omega(3))]\n', damp_ratio);
+if strcmp(analysis.damping,'rayleigh')
+    fprintf(fileID,'rayleigh $alpha 0 $beta 0 \n'); 
+elseif strcmp(analysis.damping,'modal')
+    fprintf(fileID,'modalDamping %d \n',damp_ratio); 
+    fprintf(fileID,'rayleigh 0 $beta 0 0 \n'); 
+else
+    error('Damping Type Not Recognized')
+end
+
 
 %% Close File
 fclose(fileID);
--- run_Opensees.m
+++ run_Opensees.m
@@ -5,9 +5,9 @@ clc
 
 %% DEFINE INPTUTS
 % Primary Inputs
-analysis.model_id = 4;
-analysis.gm_id = 3;
-analysis.name = 'LRHA_1.1DL_2dirGM';
+analysis.model_id = 1;
+analysis.gm_seq_id = 1;
+analysis.name = 'test';
 
 % Secondary Inputs
 analysis.type = 3;
@@ -26,9 +26,7 @@ import tools.*
 import display_model.model_plot
 
 %% Load Analysis and Model parameters
-gm_table = readtable(['inputs' filesep 'ground_motion.csv'],'ReadVariableNames',true);
 model_table = readtable(['inputs' filesep 'model.csv'],'ReadVariableNames',true);
-ground_motion = gm_table(gm_table.id == analysis.gm_id,:);
 model = model_table(model_table.id == analysis.model_id,:);
 
 %% Start Analysis
@@ -50,9 +48,9 @@ else
     error('Number of Dimensions Not Recognized')
 end
 fn_define_recorders( output_dir, analysis, node.id )
-fn_define_loads( output_dir, analysis, model.damp_ratio, node, ground_motion )
+[ground_motion] = fn_define_loads( output_dir, analysis, model.damp_ratio, node);
 fn_eigen_analysis( output_dir, analysis.time_step, story.first_story_node )
-fn_define_analysis( output_dir, analysis, node.id, ground_motion.eq_length, ground_motion.eq_dt )
+fn_define_analysis( output_dir, analysis, node.id, ground_motion )
 
 %% Run Opensees
 command = ['opensees ' output_dir filesep 'run_analysis.tcl'];
