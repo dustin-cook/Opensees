0318bafb41ce61024dcd4502e3af2e31a471182c
Dustin Cook <dustin@hbrisk.com>
2018-05-15 12:56:57 -0600

    Finalizes Moment Capacity Function

--- +asce_41/fn_aci_axial_capacity.m
+++ +asce_41/fn_aci_axial_capacity.m
@@ -2,7 +2,12 @@ function [ Pu, Pn ] = fn_aci_axial_capacity( fc, Ag, As, fy )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
-Pn = 0.8*(0.85*fc*(Ag-As)+fy*As);
+
+%% Inital Setup
+As = str2double(strsplit(strrep(strrep(As{1},'[',''),']',''),','));
+
+%% Solve for Axial Capacity
+Pn = 0.8*(0.85*fc*(Ag-sum(As))+fy*sum(As));
 phi = 0.65;
 Pu = phi*Pn;
 
--- +asce_41/fn_aci_moment_capacity.m
+++ +asce_41/fn_aci_moment_capacity.m
@@ -1,10 +1,11 @@
-function [ Mu, Mn ] = fn_aci_moment_capacity( fc, b, d, P, As, As_d, fy, clear_cover, Es )
+function [ Mu, Mn ] = fn_aci_moment_capacity( fc, b, d, As, As_d, fy, clear_cover, Es )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
 %% Inital Setup
 As = str2double(strsplit(strrep(strrep(As{1},'[',''),']',''),','));
 As_d = str2double(strsplit(strrep(strrep(As_d{1},'[',''),']',''),','));
+
 %% Calculate intermediate/defualt Values
 d_c = d - clear_cover;
 
@@ -24,16 +25,16 @@ while abs(balance_eq(count)) > tolerance
     c = d_c/2-y(count);
     e_s = abs(0.003*(As_d-c)/c);
     fs = min(e_s,fy/Es)*Es;
-    balance_eq(count) = sum(As.*fs.*((As_d-c)./abs(As_d-c))) - P - 0.85*fc*b*0.85*c;
+    balance_eq(count) = sum(As.*fs.*((As_d-c)./abs(As_d-c))) - 0.85*fc*b*0.85*c;
 end
 
-plot(y,balance_eq)
-grid on
-xlabel('Distance from Center')
-ylabel('Balance Equation')
+% plot(y,balance_eq)
+% grid on
+% xlabel('Distance from Center')
+% ylabel('Balance Equation')
 
 % Moment Capacity
-Mn = sum(As.*fs.*abs(As_d-c)) + P*(d/2-c) + 0.85*fc*b*0.85*c*(c*1.15/2);
+Mn = sum(As.*fs.*abs(As_d-c)) + 0.85*fc*b*0.85*c*(c*1.15/2);
 phi = 0.9; % Assuming tension steel strain is greater than 0.005
 Mu = phi*Mn;
 
