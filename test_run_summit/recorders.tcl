01036b60a5b7cd81ff8406ae1fd032962ff7076a
dcook519 <dustin@hbrisk.com>
2019-02-05 21:38:31 -0700

    remove video recorder

--- test_run_summit/recorders.tcl
+++ test_run_summit/recorders.tcl
@@ -3,8 +3,3 @@ recorder Node -xml test_run_summit/nodal_accel_x.xml -time -node 6000  6001  600
 recorder Node -xml test_run_summit/nodal_disp_z.xml -time -node 6000  6001  6002  6003  6004  6005  6006  6007  6008  6009  6010  6011  6012  6013  6014  6015  6016  6017 -dof 3 disp 
 recorder Node -xml test_run_summit/nodal_accel_z.xml -time -node 6000  6001  6002  6003  6004  6005  6006  6007  6008  6009  6010  6011  6012  6013  6014  6015  6016  6017 -dof 3 accel 
 recorder Element -xml test_run_summit/element_force.xml -time -ele 1  2  3  4  5  6 -dof 1 2 3 6 12 localForce 
-recorder display "Displaced shape" 10 10 500 500 -wipe 
-prp 200.0 50.0 50.0; 
-vup 0.0 1.0 0.0; 
-vpn 0.4 0.25 1; 
-display 1 5 1.000000 
