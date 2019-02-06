cb4d999056b93e4263ad83334bba4e07292c1200
dcook519 <dustin@hbrisk.com>
2019-02-05 21:27:10 -0700

    updates summit test driver

--- test_driver_summit.m
+++ test_driver_summit.m
@@ -5,7 +5,7 @@ close all
 clc
 
 disp('Matlab Open and Starting Opensees ...')
-% command = ['/projects/duco1061/software/OpenSeesSP/bin/OpenSeesSP ' opensees_dir filesep 'run_analysis.tcl'];
-command = ['opensees ' 'test_run_summit' filesep 'run_analysis.tcl'];
+command = ['/projects/duco1061/software/OpenSeesSP/bin/OpenSeesSP ' 'test_run_summit' filesep 'run_analysis.tcl'];
+% command = ['opensees ' 'test_run_summit' filesep 'run_analysis.tcl'];
 [status,cmdout] = system(command,'-echo');
 disp('Opensees Finished')
