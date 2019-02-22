d2c5ea7e257267dd21167efada28c641a71b89cc
Dustin Cook <dustin@hbrisk.com>
2019-02-22 16:33:01 -0700

    mini IDA and P-58 updates

--- mini_ida/model/set_gm_and_scale.tcl
+++ mini_ida/model/set_gm_and_scale.tcl
@@ -1,5 +1,5 @@
 puts "Defining Ground Motions and Scale ..." 
 timeSeries Path 1 -dt $dt -filePath ground_motions/ICSB_1979/chan_13_accel_15.tcl -factor 386.000000 
-pattern UniformExcitation 3 1 -accel 1 -fact 0.300000 
+pattern UniformExcitation 3 1 -accel 1 -fact 1.300000 
 timeSeries Path 2 -dt $dt -filePath ground_motions/ICSB_1979/chan_11_accel_15.tcl -factor 386.000000 
-pattern UniformExcitation 4 3 -accel 2 -fact 0.300000 
+pattern UniformExcitation 4 3 -accel 2 -fact 1.300000 
