bdcf7d5a7ff33431c0e8d70be1812b5fd75c8fb0
Dustin Cook <dustin@hbrisk.com>
2019-03-26 12:10:16 -0600

    summit

--- fn_analysis_options.m
+++ fn_analysis_options.m
@@ -75,8 +75,8 @@ elseif strcmp(analysis.proceedure,'Pushover')
     analysis.dead_load_list = [1, 1];
     analysis.live_load_list = [1, 1];
     analysis.case_list = {'NA', 'backbones'};
-    analysis.pushover_drift_list_x = [0.008, 0.012]; % Drift limit where the pushover will go till
-    analysis.pushover_drift_list_z = [0.0012, 0.0015];
+    analysis.pushover_drift_list_x = [0.008, 0.011]; % Drift limit where the pushover will go till
+    analysis.pushover_drift_list_z = [0.001, 0.0015];
     analysis.accidental_torsion_list = [0, 0];
     analysis.damp_ratio_list = [0.03, 0.03]; % Analysis damping ratio
     
