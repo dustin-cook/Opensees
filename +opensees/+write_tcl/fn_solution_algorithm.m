de1103f5f790c1ff3bcf5c66490cb26864fab8b8
Dustin Cook <dustin@hbrisk.com>
2019-04-19 16:28:20 -0600

    add algorithm variance back to solution algorithm

--- +opensees/+write_tcl/fn_solution_algorithm.m
+++ +opensees/+write_tcl/fn_solution_algorithm.m
@@ -79,7 +79,7 @@ for tol = 1:length(tolerance)
     for t = 1:length(step_reduction)
 
         % Loop Through Algorithms
-%         for a = 1:length(algorithm_typs)
+        for a = 1:length(algorithm_typs)
             fprintf(fileID,'if {$ok != 0} { \n');
 %             fprintf(fileID,'puts "analysis failed, try try tolerance = %f, step_length/%f, and algorithm = %s" \n', tolerance(tol), step_reduction(t), algorithm_typs{1});
 %             fprintf(fileID,'puts "analysis failed, try try tolerance = %f, step_length/%f" \n', tolerance(tol), step_reduction(t));
@@ -87,14 +87,14 @@ for tol = 1:length(tolerance)
             if tol <= 2
                 fprintf(fileID,'test NormDispIncr $tol %i \n', min_tolerance_steps);
 %                 fprintf(fileID,'test EnergyIncr $tol %i \n', min_tolerance_steps);
-            elseif tol <=4
+            elseif tol <=3
                 fprintf(fileID,'test NormDispIncr $tol %i \n', mid_tolerance_steps);
 %                 fprintf(fileID,'test EnergyIncr $tol %i \n', mid_tolerance_steps);
             else
                 fprintf(fileID,'test NormDispIncr $tol %i \n', max_tolerance_steps);
 %                 fprintf(fileID,'test EnergyIncr $tol %i \n', max_tolerance_steps);
             end
-%             fprintf(fileID,'algorithm %s \n', algorithm_typs{a});
+            fprintf(fileID,'algorithm %s \n', algorithm_typs{a});
             if analysis.type == 1 % Dynamic
                 fprintf(fileID,'set step_reduce %f \n', step_reduction(t));
                 fprintf(fileID,'set dt [expr %f/$step_reduce] \n', step_length);
@@ -105,7 +105,7 @@ for tol = 1:length(tolerance)
                 fprintf(fileID,'set ok [analyze 1] \n');
             end
             fprintf(fileID,'} \n');
-%         end
+        end
     end
 end
 
