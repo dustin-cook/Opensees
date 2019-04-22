a53b7e2fa3314bfc28ec962a508b72fca91b4724
Dustin Cook <dustin@hbrisk.com>
2019-04-22 08:20:23 -0600

    summit setup

--- fn_analysis_options.m
+++ fn_analysis_options.m
@@ -23,7 +23,7 @@ analysis.model_type = 2; % 1 = SDOF, 2 = MDOF (default)
 analysis.rigid_diaphram = 1; % Default the model to assume rigid diaphrams (0 = non-rigid assuption)
 
 % Opensees Analysis Options
-analysis.ground_motion_scale_factor = 1; % Scale the GM amplitude
+analysis.ground_motion_scale_factor = 0.75; % Scale the GM amplitude
 analysis.damping = 'rayleigh'; % rayleigh, modal, or simple
 analysis.hinge_stiff_mod = 10; % Scale up stiffnes of hinges for a lumped plasticiy model. n value from Ibarra paper.
 analysis.run_eigen = 1; % Run the eignen anlayis to get mode shapes and periods for the opensees analysis
@@ -102,7 +102,7 @@ elseif strcmp(analysis.proceedure,'NDP')
     analysis.pushover_drift_list_x = [0.008, 0.008, NaN]; % Drift limit where the pushover will go till
     analysis.pushover_drift_list_z = [0.0013, 0.0013, NaN];
     analysis.accidental_torsion_list = [0, 0, 1];
-    analysis.damp_ratio_list = [0.3, 0.3, 0.3]; % Analysis damping ratio
+    analysis.damp_ratio_list = [0.03, 0.03, 0.03]; % Analysis damping ratio
 %      
 %     % Just to run summit results
 %     analysis.type_list = [1]; % Linear Pushover then NL Pushover x 2 then 1 NL dynamic
