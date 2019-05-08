760b2dfd0e616f799ce335a736accd5ebb119f56
Dustin Cook <dustin@hbrisk.com>
2019-05-08 15:36:10 -0600

    more summit ida runs

--- driver_IDA.m
+++ driver_IDA.m
@@ -13,11 +13,12 @@ analysis.proceedure = 'NDP';
 analysis.id = 2;
 analysis.summit = 1;
 analysis.run_ida = 1;
+analysis.post_process_ida = 0;
 analysis.gm_set = 'FEMA_far_field';
 
 % IDA Inputs
-hazard.curve.rp = 43;%[43, 72, 224, 475, 975, 2475, 4975];
-hazard.curve.pga = 0.224;%[0.224, 0.308, 0.502, 0.635, 0.766, 0.946, 1.082];
+hazard.curve.rp = [72, 224];%[43, 72, 224, 475, 975, 2475, 4975];
+hazard.curve.pga = [0.308, 0.502];%[0.224, 0.308, 0.502, 0.635, 0.766, 0.946, 1.082];
 analysis.collapse_drift = 0.1;
 
 % Secondary options
@@ -43,9 +44,6 @@ model = model_table(model_table.id == analysis.model_id,:);
 % Import packages
 import plotting_tools.fn_format_and_save_plot
 
-% Set up Parallel Workers
-parpool;
-
 %% Define read and write directories
 model_dir = ['outputs' '/' model.name{1} '/' analysis.proceedure '_' num2str(analysis.id) '/' 'model_data'];
 tcl_dir = ['outputs' '/' model.name{1} '/' analysis.proceedure '_' num2str(analysis.id) '/' 'opensees_data'];
@@ -61,8 +59,9 @@ gm_set_table = readtable(['ground_motions' filesep analysis.gm_set filesep 'grou
 gm_median_pga = median(gm_set_table.pga);
 IDA_scale_factors = hazard.curve.pga ./ gm_median_pga;
 
-%% Run Opensees Model
+%% Run Opensees Models
 if analysis.run_ida
+    parpool; % Set up Parallel Workers
     tic
     for i = 1:length(IDA_scale_factors)
         scale_factor = IDA_scale_factors(i);
@@ -77,51 +76,51 @@ if analysis.run_ida
         end
     end
     toc
+    delete(gcp('nocreate')) % End Parallel Process
 end
 
-%% Plot Results
-plot_dir = ['outputs' '/' model.name{1} '/' analysis.proceedure '_' num2str(analysis.id) '/' 'IDA' '/' 'IDA Plots'];
-id = 0;
-for i = 1:length(IDA_scale_factors)
-    for gms = 1:height(gm_set_table)
-        for d = 1:2
-            id = id + 1;
-            % Load data
-            outputs_dir = ['outputs' '/' model.name{1} '/' analysis.proceedure '/' 'IDA' '/' 'Scale_' num2str(IDA_scale_factors(i)) '/' 'GM_' num2str(gm_set_table.set_id(gms)) '_' num2str(d)];
-            load([outputs_dir filesep 'summary_results.mat'])
-            ida.id(id,1) = id;
-            ida.scale(id,1) = IDA_scale_factors(i);
-            ida.gm_name{id,1} = gm_set_table.eq_name{gms};
-            ida.sa_x(id,1) = summary.sa_x;
-            ida.sa_z(id,1) = summary.sa_z;
-            ida.drift_x(id,1) = summary.max_drift_x;
-            ida.drift_z(id,1) = summary.max_drift_z;
-            ida.collapse(id,1) = summary.collapse;
+%% Post Processes Results
+if analysis.post_process_ida
+    % Plot Results
+    plot_dir = ['outputs' '/' model.name{1} '/' analysis.proceedure '_' num2str(analysis.id) '/' 'IDA' '/' 'IDA Plots'];
+    id = 0;
+    for i = 1:length(IDA_scale_factors)
+        for gms = 1:height(gm_set_table)
+            for d = 1:2
+                id = id + 1;
+                % Load data
+                outputs_dir = ['outputs' '/' model.name{1} '/' analysis.proceedure '_' num2str(analysis.id) '/' 'IDA' '/' 'Scale_' num2str(IDA_scale_factors(i)) '/' 'GM_' num2str(gm_set_table.set_id(gms)) '_' num2str(d)];
+                load([outputs_dir filesep 'summary_results.mat'])
+                ida.id(id,1) = id;
+                ida.scale(id,1) = IDA_scale_factors(i);
+                ida.gm_name{id,1} = gm_set_table.eq_name{gms};
+                ida.sa_x(id,1) = summary.sa_x;
+                ida.sa_z(id,1) = summary.sa_z;
+                ida.drift_x(id,1) = summary.max_drift_x;
+                ida.drift_z(id,1) = summary.max_drift_z;
+                ida.collapse(id,1) = summary.collapse;
+            end
         end
     end
+
+    % Plot IDA curves
+    fprintf('Saving IDA Summary Data and Figures to Directory: %s',plot_dir)
+    hold on
+    for gms = 1:height(gm_set_table)
+        plot(ida.drift_x(strcmp(ida.gm_name,gm_set_table.eq_name{gms})),ida.sa_x(strcmp(ida.gm_name,gm_set_table.eq_name{gms})))
+    end
+    xlabel('Max Drift')
+    ylabel('Sa(T_1) (g)')
+    fn_format_and_save_plot( plot_dir, 'IDA Plot EW Frame Direction', 2 )
+    hold on
+    for gms = 1:height(gm_set_table)
+        plot(ida.drift_z(strcmp(ida.gm_name,gm_set_table.eq_name{gms})),ida.sa_z(strcmp(ida.gm_name,gm_set_table.eq_name{gms})))
+    end
+    xlabel('Max Drift')
+    ylabel('Sa(T_1) (g)')
+    fn_format_and_save_plot( plot_dir, 'IDA Plot NS Wall Direction', 2 )
+
+    % Save results as csv
+    ida_table = struct2table(ida);
+    writetable(ida_table,[plot_dir filesep 'ida_results.csv'])
 end
-
-% Plot IDA curves
-fprintf('Saving IDA Summary Data and Figures to Directory: %s',plot_dir)
-hold on
-for gms = 1:height(gm_set_table)
-    plot(ida.drift_x(strcmp(ida.gm_name,gm_set_table.eq_name{gms})),ida.sa_x(strcmp(ida.gm_name,gm_set_table.eq_name{gms})))
-end
-xlabel('Max Drift')
-ylabel('Sa(T_1) (g)')
-fn_format_and_save_plot( plot_dir, 'IDA Plot EW Frame Direction', 2 )
-hold on
-for gms = 1:height(gm_set_table)
-    plot(ida.drift_z(strcmp(ida.gm_name,gm_set_table.eq_name{gms})),ida.sa_z(strcmp(ida.gm_name,gm_set_table.eq_name{gms})))
-end
-xlabel('Max Drift')
-ylabel('Sa(T_1) (g)')
-fn_format_and_save_plot( plot_dir, 'IDA Plot NS Wall Direction', 2 )
-
-% Save results as csv
-ida_table = struct2table(ida);
-writetable(ida_table,[plot_dir filesep 'ida_results.csv'])
-
-%% End Parallel Process
-delete(gcp('nocreate'))
-
