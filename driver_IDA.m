06c8056c110c87674ec835fbeeab32e33748425d
dcook519 <dustin@hbrisk.com>
2019-05-15 07:40:45 -0600

    add back parallelization

--- driver_IDA.m
+++ driver_IDA.m
@@ -13,12 +13,12 @@ analysis.proceedure = 'NDP';
 analysis.id = 3;
 analysis.summit = 1;
 analysis.run_ida = 1;
-analysis.post_process_ida = 1;
+analysis.post_process_ida = 0;
 analysis.gm_set = 'FEMA_far_field';
 
 % IDA Inputs
-hazard.curve.rp = [43, 72, 224, 475, 975, 2475, 4975];%[43, 72, 224, 475, 975, 2475, 4975];
-hazard.curve.pga = [0.224, 0.308, 0.502, 0.635, 0.766, 0.946, 1.082];%[0.224, 0.308, 0.502, 0.635, 0.766, 0.946, 1.082];
+hazard.curve.rp = [224, 475, 975, 2475, 4975];%[43, 72, 224, 475, 975, 2475, 4975];
+hazard.curve.pga = [0.502, 0.635, 0.766, 0.946, 1.082];%[0.224, 0.308, 0.502, 0.635, 0.766, 0.946, 1.082];
 analysis.collapse_drift = 0.05;
 
 % Secondary options
@@ -61,12 +61,12 @@ IDA_scale_factors = hazard.curve.pga ./ gm_median_pga;
 
 %% Run Opensees Models
 if analysis.run_ida
-%     parpool; % Set up Parallel Workers
+    parpool; % Set up Parallel Workers
     tic
     for i = 1:length(IDA_scale_factors)
         error_count = 0;
         scale_factor = IDA_scale_factors(i);
-        for gms = 1:height(gm_set_table)
+        parfor gms = 1:height(gm_set_table)
             % Suppress MATLAB warnings
             warning('off','all')
             
@@ -81,7 +81,7 @@ if analysis.run_ida
         fprintf('%i Failed GMs for Scale Factor %4.2f \n\n', error_count, scale_factor)
     end
     toc
-%     delete(gcp('nocreate')) % End Parallel Process
+    delete(gcp('nocreate')) % End Parallel Process
 end
 
 %% Post Processes Results
@@ -89,6 +89,7 @@ if analysis.post_process_ida
     % Plot Results
     plot_dir = ['outputs' '/' model.name{1} '/' analysis.proceedure '_' num2str(analysis.id) '/' 'IDA' '/' 'IDA Plots'];
     id = 0;
+    id_2 = 0;
     for i = 1:length(IDA_scale_factors)
         for gms = 1:height(gm_set_table)   
             % Load data
@@ -105,6 +106,11 @@ if analysis.post_process_ida
                 ida.drift_x(id,1) = summary.max_drift_x;
                 ida.drift_z(id,1) = summary.max_drift_z;
                 ida.collapse(id,1) = summary.collapse;
+            else
+                id_2 = id_2 + 1;
+                missing_ida.scale(id_2,1) = IDA_scale_factors(i);
+                missing_ida.gm_set_id(id_2,1) = gm_set_table.set_id(gms);
+                missing_ida.gm_set_pair_id(id_2,1) = gm_set_table.pair(gms);
             end
         end
     end
