759390c33951c78a7aa7475426c1d168b4fe88a4
Dustin Cook <dustin@hbrisk.com>
2019-05-29 16:26:34 -0600

    test newton on summit

--- driver_ASCE_41_teir_3_assessment.m
+++ driver_ASCE_41_teir_3_assessment.m
@@ -18,7 +18,7 @@ fclose('all');
 %% User Inputs (Think about changing this to a file read and command line execution)
 analysis.model_id = 11;
 analysis.proceedure = 'NDP'; % LDP or NDP or test
-analysis.id = 21; % ID of the analysis for it to create its own directory
+analysis.id = 22; % ID of the analysis for it to create its own directory
 analysis.summit = 1; % Write tcl files to be run on summit and change location of opensees call
 analysis.gm_seq_id = 12; % Maybe also make this part ot the defaults or model?
 
